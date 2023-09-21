6bb05ed7c031d253d4fdb7b1426a4a612ead5a3b
Sushan M <91654089+Sushan-M@users.noreply.github.com>
2023-09-21 15:30:13 +0530

    changed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Layout from "./pages/Layout";
 import Home from "./pages/Home";
 import './App.css';
@@ -8,14 +8,14 @@ import Games from "./pages/Game";
 function App() {
   return (
     //basename={process.env.PUBLIC_URL}
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" exact element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="games" element={<Games />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
